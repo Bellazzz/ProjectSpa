74e06b4291753d51a71843f003e159eddd02bb0f
Bellazzz <mewseedza@gmail.com>
2014-08-25 14:10:08 +0700

    tem_c

---
+++ backoffice/template_c/47ef1ecefa72341855e478a830fc88e218470c04.file.form_service_list_types.html.php
@@ -0,0 +1,82 @@
+<?php /* Smarty version Smarty-3.1.18, created on 2014-08-25 15:09:41
+         compiled from "C:\AppServ\www\projectSpa\backoffice\template\form_service_list_types.html" */ ?>
+<?php /*%%SmartyHeaderCode:476453fae135233682-37313900%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '47ef1ecefa72341855e478a830fc88e218470c04' => 
+    array (
+      0 => 'C:\\AppServ\\www\\projectSpa\\backoffice\\template\\form_service_list_types.html',
+      1 => 1408950531,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '476453fae135233682-37313900',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'action' => 0,
+    'tableName' => 0,
+    'tableNameTH' => 0,
+    'code' => 0,
+    'values' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.18',
+  'unifunc' => 'content_53fae1353206a2_50863415',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_53fae1353206a2_50863415')) {function content_53fae1353206a2_50863415($_smarty_tpl) {?><!DOCTYPE html>
+<html lang="th">
+<head>
+	<title>Spa - Backoffice</title>
+	<meta charset="UTF-8"/>
+    
+	<link rel="stylesheet" type="text/css" href="../inc/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/lazybingo.css">
+    <script type="text/javascript" src="../js/jquery.min.js"></script>
+    <script type="text/javascript" src="../js/mbk_common_function.js"></script>
+    <script type="text/javascript" src="../js/mbk_main.js"></script>
+    <script type="text/javascript" src="../js/mbk_form_table.js"></script>
+    <script type="text/javascript">
+        // Global variables
+        var action      = '<?php echo $_smarty_tpl->tpl_vars['action']->value;?>
+';
+        var tableName   = '<?php echo $_smarty_tpl->tpl_vars['tableName']->value;?>
+';
+		var tableNameTH = '<?php echo $_smarty_tpl->tpl_vars['tableNameTH']->value;?>
+';
+        var code        = '<?php echo $_smarty_tpl->tpl_vars['code']->value;?>
+';
+        var ajaxUrl     = 'form_table.php';
+    </script>
+    
+</head>
+<body>
+ 	 	 	 	 
+<?php echo $_smarty_tpl->getSubTemplate ("form_table_header.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
+
+<div class="ftb-body">
+    <form id="form-table" name="form-table" onsubmit="return false;">
+	<input type="hidden" name="requiredFields" value="svltyp_name">
+	<input type="hidden" name="uniqueFields" value="svltyp_name">
+    <table class="mbk-form-input-normal" cellpadding="0" cellspacing="0">
+	    <tbody>
+		    <tr>
+			    <td>
+				    <label class="input-required">ชื่อประเภทรายการบริการ</label>
+				    <input id="svltyp_name" name="svltyp_name" type="text" class="form-input full" value="<?php echo $_smarty_tpl->tpl_vars['values']->value['svltyp_name'];?>
+">
+			    </td>
+	    </tbody>
+    </table>
+    </form>
+</div>
+</body>
+</html>
+<!--
+    [Note]
+    1. ให้ใส่ field ที่ต้องการเช็คใน input[name="requiredFields"] โดยกำหนดชื่อฟิลด์ลงใน value หากมีมากกว่า 1 field ให้คั่นด้วยเครื่องหมาย คอมม่า (,) และห้ามมีช่องว่าง เช่น value="name,surname,address" เป็นต้น
+    2. input จะต้องกำหนด id, name ให้ตรงกับชื่อฟิลด์ของตารางนั้นๆ และกำหนด value ให้มีรูปแบบ value="$values.ชื่อฟิลด์"
+--><?php }} ?>
