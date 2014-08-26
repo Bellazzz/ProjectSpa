2b58f39c8a264f4efeff217c610ad8150151c405
Mamew <mewseedza@gmail.com>
2014-08-26 20:44:13 +0700

    Edit frm service done

--- backoffice/template_c/36d75e95668141b453660122c4bdc7ac8928024e.file.form_booking_service_lists.html.php
+++ backoffice/template_c/36d75e95668141b453660122c4bdc7ac8928024e.file.form_booking_service_lists.html.php
@@ -1,9 +1,38 @@
-<!DOCTYPE html>
+<?php /* Smarty version Smarty-3.1.18, created on 2014-08-26 21:41:54
+         compiled from "C:\AppServ\www\projectSpa\backoffice\template\form_booking_service_lists.html" */ ?>
+<?php /*%%SmartyHeaderCode:2102753fc8987a24679-19481296%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '36d75e95668141b453660122c4bdc7ac8928024e' => 
+    array (
+      0 => 'C:\\AppServ\\www\\projectSpa\\backoffice\\template\\form_booking_service_lists.html',
+      1 => 1409060502,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '2102753fc8987a24679-19481296',
+  'function' => 
+  array (
+  ),
+  'version' => 'Smarty-3.1.18',
+  'unifunc' => 'content_53fc8987b30886_38325152',
+  'variables' => 
+  array (
+    'action' => 0,
+    'tableName' => 0,
+    'tableNameTH' => 0,
+    'code' => 0,
+    'values' => 0,
+  ),
+  'has_nocache_code' => false,
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_53fc8987b30886_38325152')) {function content_53fc8987b30886_38325152($_smarty_tpl) {?><!DOCTYPE html>
 <html lang="th">
 <head>
 	<title>Spa - Backoffice</title>
 	<meta charset="UTF-8"/>
-    {literal}
+    
 	<link rel="stylesheet" type="text/css" href="../inc/font-awesome/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="../css/lazybingo.css">
 	<link rel="stylesheet" type="text/css" href="../inc/jquery-ui/jquery-ui.css"> <!--include if want to use datepicker-->
@@ -14,10 +43,14 @@
     <script type="text/javascript" src="../js/mbk_form_table.js"></script>
     <script type="text/javascript">
         // Global variables
-        var action      = '{/literal}{$action}{literal}';
-        var tableName   = '{/literal}{$tableName}{literal}';
-		var tableNameTH = '{/literal}{$tableNameTH}{literal}';
-        var code        = '{/literal}{$code}{literal}';
+        var action      = '<?php echo $_smarty_tpl->tpl_vars['action']->value;?>
+';
+        var tableName   = '<?php echo $_smarty_tpl->tpl_vars['tableName']->value;?>
+';
+		var tableNameTH = '<?php echo $_smarty_tpl->tpl_vars['tableNameTH']->value;?>
+';
+        var code        = '<?php echo $_smarty_tpl->tpl_vars['code']->value;?>
+';
         var ajaxUrl     = 'form_table.php';
 
         $(document).ready(function () {
@@ -27,7 +60,8 @@
                 keyFieldName	: 'svl_id',
                 textFieldName	: 'svl_name',
 				searchTool		: true,
-                defaultValue	: '{/literal}{$values.svl_id}{literal}',
+                defaultValue	: '<?php echo $_smarty_tpl->tpl_vars['values']->value['svl_id'];?>
+',
 				
             });
              
@@ -37,17 +71,19 @@
                 keyFieldName    : 'bkg_id',
                 textFieldName   : 'bkg_id',
                 searchTool      : true,
-                defaultValue    : '{/literal}{$values.bkg_id}{literal}'
+                defaultValue    : '<?php echo $_smarty_tpl->tpl_vars['values']->value['bkg_id'];?>
+'
             });
 
 			$("#bkgsvl_date").datepicker();
         });
     </script>
-    {/literal}
+    
 </head>
 <body>
 
-{include file="form_table_header.html"}
+<?php echo $_smarty_tpl->getSubTemplate ("form_table_header.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
+
 <div class="ftb-body">
     <form id="form-table" name="form-table" onsubmit="return false;">
 	<input type="hidden" name="requiredFields" value="svl_id,bkg_id,bkgsvl_date,bkgsvl_total_price,bkgsvl_persons">
@@ -68,17 +104,20 @@
 			<tr>
                 <td>
                     <label class="input-required">วัน-เวลาที่ใช้บริการ</label>
-				      <input id="bkgsvl_date" name="bkgsvl_date" type="text" class="form-input half" value="{$values.bkgsvl_date}">
+				      <input id="bkgsvl_date" name="bkgsvl_date" type="text" class="form-input half" value="<?php echo $_smarty_tpl->tpl_vars['values']->value['bkgsvl_date'];?>
+">
 			    </td>
 			    <td>
 				    <label class="input-required">ราคารวทการจองรายการบริการ(บาท)</label>
-				    <input id="bkgsvl_total_price" name="bkgsvl_total_price" type="text" class="form-input half" value="{$values.bkgsvl_total_price}">
+				    <input id="bkgsvl_total_price" name="bkgsvl_total_price" type="text" class="form-input half" value="<?php echo $_smarty_tpl->tpl_vars['values']->value['bkgsvl_total_price'];?>
+">
 			    </td>
 		    </tr>
             <tr>
                 <td>
                     <label class="input-required">จำนวนผู้ใช้บริการ</label>
-                    <input id="bkgsvl_persons" name="bkgsvl_persons" type="text" class="form-input half" value="{$values.bkgsvl_persons}">
+                    <input id="bkgsvl_persons" name="bkgsvl_persons" type="text" class="form-input half" value="<?php echo $_smarty_tpl->tpl_vars['values']->value['bkgsvl_persons'];?>
+">
                 </td>
                 
             </tr>
@@ -94,4 +133,4 @@
     1. ให้ใส่ field ที่ต้องการเช็คใน input[name="requiredFields"] โดยกำหนดชื่อฟิลด์ลงใน value หากมีมากกว่า 1 field ให้คั่นด้วยเครื่องหมาย คอมม่า (,) และห้ามมีช่องว่าง เช่น value="name,surname,address" เป็นต้น
     2. input จะต้องกำหนด id, name ให้ตรงกับชื่อฟิลด์ของตารางนั้นๆ และกำหนด value ให้มีรูปแบบ value="$values.ชื่อฟิลด์"
 	3.  input[name="uniqueFields"] ใส่ชื่อฟิลด์ที่ต้องการเช็คว่าห้ามซ้ำ
--->
+--><?php }} ?>
--- backoffice/template_c/7f6b5fe50418d9648bdbbe8a9fee3ac0520b3718.file.form_payrolls.html.php
+++ backoffice/template_c/7f6b5fe50418d9648bdbbe8a9fee3ac0520b3718.file.form_payrolls.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.18, created on 2014-08-26 20:11:40
+<?php /* Smarty version Smarty-3.1.18, created on 2014-08-26 21:33:50
          compiled from "C:\AppServ\www\projectSpa\backoffice\template\form_payrolls.html" */ ?>
 <?php /*%%SmartyHeaderCode:2770453fc715011d650-54455661%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '7f6b5fe50418d9648bdbbe8a9fee3ac0520b3718' => 
     array (
       0 => 'C:\\AppServ\\www\\projectSpa\\backoffice\\template\\form_payrolls.html',
-      1 => 1409055043,
+      1 => 1409060024,
       2 => 'file',
     ),
   ),
