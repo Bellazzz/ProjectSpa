08121669a7788a57eb12c367f2acaf28f4f2524f
Mamew <mewseedza@gmail.com>
2014-08-31 20:04:28 +0700

    เบลกากกกกก

---
+++ backoffice/template_c/0cc5612f2b8da9aa71c09fc4bfb6d25d4a9518cf.file.form_table_header.html.php
@@ -0,0 +1,50 @@
+<?php /* Smarty version Smarty-3.1.18, created on 2014-08-31 21:01:32
+         compiled from "C:\AppServ\www\projectSpa\backoffice\template\form_table_header.html" */ ?>
+<?php /*%%SmartyHeaderCode:1334754031cacb59058-76425545%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '0cc5612f2b8da9aa71c09fc4bfb6d25d4a9518cf' => 
+    array (
+      0 => 'C:\\AppServ\\www\\projectSpa\\backoffice\\template\\form_table_header.html',
+      1 => 1408871450,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '1334754031cacb59058-76425545',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'action' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.18',
+  'unifunc' => 'content_54031cacbf89a1_51175770',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_54031cacbf89a1_51175770')) {function content_54031cacbf89a1_51175770($_smarty_tpl) {?><div class="ftb-header">
+	<div class="title-container">
+		<h1 id="ftb-title"></h1>
+	</div>
+    <div class="toolbar-container clearfix">
+	    <ul class="toolbar-menu">
+		    <li>
+                <?php if ($_smarty_tpl->tpl_vars['action']->value=='ADD') {?>
+			    <a id="save-btn">
+				    <i class="fa fa-check"></i> เพิ่ม
+			    </a>
+                <?php } elseif ($_smarty_tpl->tpl_vars['action']->value=='EDIT') {?>
+                <a id="save-btn">
+				    <i class="fa fa-floppy-o"></i> บันทึก
+			    </a>
+                 <?php }?>
+		    </li>
+		    <li>
+			    <a id="cancel-btn">
+				    <i class="fa fa-times"></i> ยกเลิก
+			    </a>
+		    </li>
+	    </ul>
+    </div>
+</div><?php }} ?>
