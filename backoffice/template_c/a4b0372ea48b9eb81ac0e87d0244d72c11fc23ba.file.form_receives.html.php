9de3790547b57ee8f22b97c72882e31be97b2452
Mamew <mewseedza@gmail.com>
2014-09-25 21:55:58 +0700

    edit date thai

--- backoffice/template_c/a4b0372ea48b9eb81ac0e87d0244d72c11fc23ba.file.form_receives.html.php
+++ backoffice/template_c/a4b0372ea48b9eb81ac0e87d0244d72c11fc23ba.file.form_receives.html.php
@@ -1,9 +1,47 @@
-<!DOCTYPE html>
+<?php /* Smarty version Smarty-3.1.18, created on 2014-09-25 22:41:25
+         compiled from "C:\AppServ\www\projectSpa\backoffice\template\form_receives.html" */ ?>
+<?php /*%%SmartyHeaderCode:1024154242082a85aa6-95182787%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'a4b0372ea48b9eb81ac0e87d0244d72c11fc23ba' => 
+    array (
+      0 => 'C:\\AppServ\\www\\projectSpa\\backoffice\\template\\form_receives.html',
+      1 => 1411656073,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '1024154242082a85aa6-95182787',
+  'function' => 
+  array (
+  ),
+  'version' => 'Smarty-3.1.18',
+  'unifunc' => 'content_54242082da6023_76588523',
+  'variables' => 
+  array (
+    'action' => 0,
+    'tableName' => 0,
+    'tableNameTH' => 0,
+    'code' => 0,
+    'nowDate' => 0,
+    'ord_date' => 0,
+    'values' => 0,
+    'session_emp_id' => 0,
+    'receiveDetailList' => 0,
+    'recdlt' => 0,
+    'sum_amount' => 0,
+    'recPrdList' => 0,
+    'recPrd' => 0,
+    'ordPrdList' => 0,
+  ),
+  'has_nocache_code' => false,
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_54242082da6023_76588523')) {function content_54242082da6023_76588523($_smarty_tpl) {?><!DOCTYPE html>
 <html lang="th">
 <head>
 	<title>Spa - Backoffice</title>
 	<meta charset="UTF-8"/>
-    {literal}
+    
 	<link rel="stylesheet" type="text/css" href="../inc/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="../css/lazybingo.css">
     <!--<link rel="stylesheet" type="text/css" href="../inc/jquery-ui/jquery-ui.css">-->
@@ -59,13 +97,19 @@
     </style>
     <script type="text/javascript">
         // Global variables
-        var action      = '{/literal}{$action}{literal}';
-        var tableName   = '{/literal}{$tableName}{literal}';
-		var tableNameTH = '{/literal}{$tableNameTH}{literal}';
-        var code        = '{/literal}{$code}{literal}';
+        var action      = '<?php echo $_smarty_tpl->tpl_vars['action']->value;?>
+';
+        var tableName   = '<?php echo $_smarty_tpl->tpl_vars['tableName']->value;?>
+';
+		var tableNameTH = '<?php echo $_smarty_tpl->tpl_vars['tableNameTH']->value;?>
+';
+        var code        = '<?php echo $_smarty_tpl->tpl_vars['code']->value;?>
+';
         var ajaxUrl     = 'form_receives.php';
-        var nowDate     = '{/literal}{$nowDate}{literal}';
-        var ordDate     = '{/literal}{$ord_date}{literal}';
+        var nowDate     = '<?php echo $_smarty_tpl->tpl_vars['nowDate']->value;?>
+';
+        var ordDate     = '<?php echo $_smarty_tpl->tpl_vars['ord_date']->value;?>
+';
 
         $(document).ready(function () {
             selectReference({
@@ -74,7 +118,8 @@
                 keyFieldName    : 'ord_id',
                 textFieldName   : 'ord_id',
                 searchTool      : true,
-                defaultValue    : '{/literal}{$values.ord_id}{literal}',
+                defaultValue    : '<?php echo $_smarty_tpl->tpl_vars['values']->value['ord_id'];?>
+',
                 condition       : "ordstat_id != 'OS03'",
                 onOptionSelect  : changeOrderId,
                 allowChangeOption : allowChangeOrderId
@@ -86,7 +131,9 @@
                 keyFieldName    : 'emp_id',
                 textFieldName   : 'emp_id,emp_name,emp_surname',
                 searchTool      : true,
-                defaultValue    : '{/literal}{if $values.emp_id}{$values.emp_id}{else}{$session_emp_id}{/if}{literal}',
+                defaultValue    : '<?php if ($_smarty_tpl->tpl_vars['values']->value['emp_id']) {?><?php echo $_smarty_tpl->tpl_vars['values']->value['emp_id'];?>
+<?php } else { ?><?php echo $_smarty_tpl->tpl_vars['session_emp_id']->value;?>
+<?php }?>',
                 pattern         : 'CONCAT("(",emp_id,") ",emp_name," ",emp_surname)'
             });
              
@@ -131,13 +178,14 @@
             ordDate = date;
         }
     </script>
-    {/literal}
+    
 </head>
 <body>
 
-{include file="form_table_header.html"}
+<?php echo $_smarty_tpl->getSubTemplate ("form_table_header.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
+
 <div class="ftb-body">  
-    {if $action == 'VIEW_DETAIL'}
+    <?php if ($_smarty_tpl->tpl_vars['action']->value=='VIEW_DETAIL') {?>
     <!-- VIEW_DETAIL -->
     <!--Orders Data-->
     <label class="article-title">ข้อมูลการรับ</label>
@@ -145,12 +193,15 @@
         <tbody>
             <tr>
                 <td>รหัสการรับ :</td>
-                <td>{$code}</td>
+                <td><?php echo $_smarty_tpl->tpl_vars['code']->value;?>
+</td>
             </tr>
              <tr>
                 <td>รหัสการสั่งซื้อ :</td>
                 <td>
-                    <a class="normal-link" href="form_orders.php?action=VIEW_DETAIL&code={$values.ord_id}" title="คลิกเพื่อดูรายละเอียด">{$values.ord_id}</a>
+                    <a class="normal-link" href="form_orders.php?action=VIEW_DETAIL&code=<?php echo $_smarty_tpl->tpl_vars['values']->value['ord_id'];?>
+" title="คลิกเพื่อดูรายละเอียด"><?php echo $_smarty_tpl->tpl_vars['values']->value['ord_id'];?>
+</a>
                 </td>
             </tr>
             <tr>
@@ -159,7 +210,8 @@
             </tr>
              <tr>
                 <td>วันที่รับผลิตภัณฑ์ :</td>
-                <td>{$values.rec_date_th}</td>
+                <td><?php echo $_smarty_tpl->tpl_vars['values']->value['rec_date_th'];?>
+</td>
             </tr>
         </tbody>
     </table>
@@ -176,25 +228,38 @@
             </tr>
         </thead>
         <tbody>
-            {foreach from=$receiveDetailList key=i item=recdlt}
+            <?php  $_smarty_tpl->tpl_vars['recdlt'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['recdlt']->_loop = false;
+ $_smarty_tpl->tpl_vars['i'] = new Smarty_Variable;
+ $_from = $_smarty_tpl->tpl_vars['receiveDetailList']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+foreach ($_from as $_smarty_tpl->tpl_vars['recdlt']->key => $_smarty_tpl->tpl_vars['recdlt']->value) {
+$_smarty_tpl->tpl_vars['recdlt']->_loop = true;
+ $_smarty_tpl->tpl_vars['i']->value = $_smarty_tpl->tpl_vars['recdlt']->key;
+?>
             <tr>
-                <td>{$recdlt.prd_name}</td>
-                <td align="left">{$recdlt.unit_name}</td>
-                <td align="right">{$recdlt.recdtl_price|number_format:2:".":","}</td>
-                <td align="right">{$recdlt.recdtl_amount|number_format:0:"":","}</td>
-                <td align="right">{$recdlt.sum_price}</td>
+                <td><?php echo $_smarty_tpl->tpl_vars['recdlt']->value['prd_name'];?>
+</td>
+                <td align="left"><?php echo $_smarty_tpl->tpl_vars['recdlt']->value['unit_name'];?>
+</td>
+                <td align="right"><?php echo number_format($_smarty_tpl->tpl_vars['recdlt']->value['recdtl_price'],2,".",",");?>
+</td>
+                <td align="right"><?php echo number_format($_smarty_tpl->tpl_vars['recdlt']->value['recdtl_amount'],0,'',",");?>
+</td>
+                <td align="right"><?php echo $_smarty_tpl->tpl_vars['recdlt']->value['sum_price'];?>
+</td>
             </tr>
-            {/foreach}
+            <?php } ?>
         </tbody>
         <tfoot>
             <tr class="tfootSummary">
                 <td align="right" colspan="3">รวมทั้งหมด</td>
-                <td align="right"><b>{$sum_amount|number_format:0:"":","}</b></td>
-                <td align="right"><b>{$values.rec_total_price|number_format:2:".":","}</b></td>
+                <td align="right"><b><?php echo number_format($_smarty_tpl->tpl_vars['sum_amount']->value,0,'',",");?>
+</b></td>
+                <td align="right"><b><?php echo number_format($_smarty_tpl->tpl_vars['values']->value['rec_total_price'],2,".",",");?>
+</b></td>
             </tr>
         </tfoot>
     </table>
-    {else}      
+    <?php } else { ?>      
     <!-- ADD, EDIT -->    
     <form id="form-table" name="form-table" onsubmit="return false;">
 	<input type="hidden" name="requiredFields" value="ord_id,emp_id,rec_date,rec_total_price">
@@ -211,13 +276,15 @@
                 </td>
                 <td class="samerow">
                     <label class="input-required">วันที่รับผลิตภัณฑ์</label>
-                      <input id="rec_date" name="rec_date" type="text" class="mbk-dtp-th form-input half" value="{if $values.rec_date}{$values.rec_date}{else}{$nowDate}{/if}">
+                      <input id="rec_date" name="rec_date" type="text" class="mbk-dtp-th form-input half" value="<?php if ($_smarty_tpl->tpl_vars['values']->value['rec_date']) {?><?php echo $_smarty_tpl->tpl_vars['values']->value['rec_date'];?>
+<?php } else { ?><?php echo $_smarty_tpl->tpl_vars['nowDate']->value;?>
+<?php }?>">
                 </td>
             </tr>
 	    </tbody>
     </table>
     <div id="receives-product-list-container">
-    {if $action == 'EDIT'}
+    <?php if ($_smarty_tpl->tpl_vars['action']->value=='EDIT') {?>
         <table id="receives-product-list">
             <tr>
                 <th>ลำดับ</th>
@@ -229,27 +296,42 @@
                 <th>ราคารวม</th>
                 <th>คงเหลือ</th>
             </tr>
-            {foreach from=$recPrdList key=i item=recPrd}
+            <?php  $_smarty_tpl->tpl_vars['recPrd'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['recPrd']->_loop = false;
+ $_smarty_tpl->tpl_vars['i'] = new Smarty_Variable;
+ $_from = $_smarty_tpl->tpl_vars['recPrdList']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+foreach ($_from as $_smarty_tpl->tpl_vars['recPrd']->key => $_smarty_tpl->tpl_vars['recPrd']->value) {
+$_smarty_tpl->tpl_vars['recPrd']->_loop = true;
+ $_smarty_tpl->tpl_vars['i']->value = $_smarty_tpl->tpl_vars['recPrd']->key;
+?>
             <tr>
                 <td align="center">
-                    {$recPrd.no}
-                    <input type="hidden" name="prd_id[]" value="{$recPrd.prd_id}">
-                    <input type="hidden" name="recdtl_id[]" value="{$recPrd.recdtl_id}">
+                    <?php echo $_smarty_tpl->tpl_vars['recPrd']->value['no'];?>
+
+                    <input type="hidden" name="prd_id[]" value="<?php echo $_smarty_tpl->tpl_vars['recPrd']->value['prd_id'];?>
+">
+                    <input type="hidden" name="recdtl_id[]" value="<?php echo $_smarty_tpl->tpl_vars['recPrd']->value['recdtl_id'];?>
+">
                 </td>
                 <td>
-                    <span class="prd_name">{$recPrd.prd_name}</span>
+                    <span class="prd_name"><?php echo $_smarty_tpl->tpl_vars['recPrd']->value['prd_name'];?>
+</span>
                 </td>
                 <td align="right">
-                    <span class="ordPrd_amount">{$ordPrdList[$recPrd.prd_id].amount}</span>
+                    <span class="ordPrd_amount"><?php echo $_smarty_tpl->tpl_vars['ordPrdList']->value[$_smarty_tpl->tpl_vars['recPrd']->value['prd_id']]['amount'];?>
+</span>
                 </td>
                 <td align="center">
-                    <span class="unit_name">{$recPrd.unit_name}</span>
+                    <span class="unit_name"><?php echo $_smarty_tpl->tpl_vars['recPrd']->value['unit_name'];?>
+</span>
                 </td>
                 <td>
-                    <input type="text" name="recdtl_amount[]" value="{$recPrd.recdtl_amount}" min="0" max="{$ordPrdList[$recPrd.prd_id].amount}">
+                    <input type="text" name="recdtl_amount[]" value="<?php echo $_smarty_tpl->tpl_vars['recPrd']->value['recdtl_amount'];?>
+" min="0" max="<?php echo $_smarty_tpl->tpl_vars['ordPrdList']->value[$_smarty_tpl->tpl_vars['recPrd']->value['prd_id']]['amount'];?>
+">
                 </td>
                 <td>
-                    <input type="text" name="recdtl_price[]" value="{$recPrd.recdtl_price}">
+                    <input type="text" name="recdtl_price[]" value="<?php echo $_smarty_tpl->tpl_vars['recPrd']->value['recdtl_price'];?>
+">
                 </td>
                 <td align="right">
                     <span class="sum_price"></span>
@@ -258,7 +340,7 @@
                     <span class="remain"></span>
                 </td>
             </tr>
-            {/foreach}
+            <?php } ?>
             <tr>
                 <td colspan="6" align="right">
                     รวม
@@ -272,10 +354,10 @@
                 </td>
             </tr>
         </table>
-    {/if}
+    <?php }?>
     </div>
     </form>
-    {/if}
+    <?php }?>
 </div>
 </body>
 </html>
@@ -284,4 +366,4 @@
     1. ให้ใส่ field ที่ต้องการเช็คใน input[name="requiredFields"] โดยกำหนดชื่อฟิลด์ลงใน value หากมีมากกว่า 1 field ให้คั่นด้วยเครื่องหมาย คอมม่า (,) และห้ามมีช่องว่าง เช่น value="name,surname,address" เป็นต้น
     2. input จะต้องกำหนด id, name ให้ตรงกับชื่อฟิลด์ของตารางนั้นๆ และกำหนด value ให้มีรูปแบบ value="$values.ชื่อฟิลด์"
 	3.  input[name="uniqueFields"] ใส่ชื่อฟิลด์ที่ต้องการเช็คว่าห้ามซ้ำ
--->
+--><?php }} ?>
